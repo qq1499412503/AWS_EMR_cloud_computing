965c2eddf0db539168ddf6c1f20801653cf534d6
Bobby <qq1499412503@gmail.com>
2020-10-13 11:36:36 +1100

    Delete bootstrap.sh

--- Cloud_Computing_Assignment/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data all
-sudo pip-3.6 install --quiet tensorflow-hub
-sudo pip-3.6 install --quiet matplotlib
-sudo pip-3.6 install --quiet pandas
